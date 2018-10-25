98a1d6b47dcd930d4bcc47f09af45a65704e94a2
boktah <boktah2@gmail.com>
2018-10-25 18:22:53 -0400

    removed unused imports

--- CleanTest/src/org/usfirst/frc/team5518/robot/Robot.java
+++ CleanTest/src/org/usfirst/frc/team5518/robot/Robot.java
@@ -10,9 +10,6 @@ package org.usfirst.frc.team5518.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import org.usfirst.frc.team5518.robot.subsystems.ExampleSubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the
